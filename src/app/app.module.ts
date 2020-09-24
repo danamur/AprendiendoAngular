fa7291a1ce6315de2ede04e6c31ea8c50aa0fdff
danamur <59931318+danamur@users.noreply.github.com>
2020-09-23 23:08:47 -0300

    Add files via upload

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { MiComponente } from './components/miComponente/miComponente.component';
+import { PeliculasComponent } from './components/peliculas/peliculas.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MiComponente,
+    PeliculasComponent
   ],
   imports: [
     BrowserModule
